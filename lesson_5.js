3bc77833a7755ecec7f1038532697e0f1c91508e
אוירבך נעמי <152@ladaat.local>
2024-11-12 17:39:47 +0200

    begin

---
+++ lesson_5.js
@@ -0,0 +1,12 @@
+//מספר אחד
+for (let i = 0; i < catalog.length; i++) {
+    catalog[i]["id"]=i+100
+    
+}
+console.log(catalog)
+//מספר 2
+ function f1(str) {
+    for (let i = 0; i < catalog.length; i++) {
+       if
+    }
+ }
